a7fb81563a353c478c674fd48ce2fa1b4c9eca55
DesiAPG <desireapgames@gmail.com>
2022-02-22 19:12:50 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import NewComponent from "./NewComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NewComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
