a27f6ab2a46f7a986354555f803a6051312654e6
DesiAPG <desireapgames@gmail.com>
2022-02-27 20:38:22 -0500

    fixed-price

--- src/App.js
+++ src/App.js
@@ -1,14 +1,106 @@
 import "./App.css";
+import { useState } from "react";
+import atanagildo from "./reyes/rey_atanagildo.png";
+import ervigio from "./reyes/rey_ervigio.png";
+import ataulfo from "./reyes/rey_ataulfo.png";
+import leogivildo from "./reyes/rey_leogivildo.png";
+import recesvinto from "./reyes/rey_recesvinto.png";
+import sisebuto from "./reyes/rey_sisebuto.png";
 import NewComponent from "./NewComponent";
 
-function App() {
+const App = () => {
+  const [total, setTotal] = useState(0);
+
+  const kings = [
+    {
+      name: "Atanagildo",
+      color: "darkolivegreen",
+      price: 178,
+    },
+    {
+      name: "Ervigio",
+      color: "crimson",
+      price: 169,
+    },
+    {
+      name: "Ataúlfo",
+      color: "peru",
+      price: 81,
+    },
+    {
+      name: "Leogivildo",
+      color: "darkmagenta",
+      price: 126,
+    },
+    {
+      name: "Recesvinto",
+      color: "royalblue",
+      price: 141,
+    },
+    {
+      name: "Sisebuto",
+      color: "teal",
+      price: 69,
+    },
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <NewComponent />
+        <h1>Total to pay: ${total}</h1>
+        <div className="container">
+          <div className="card atanagildo">
+            <NewComponent
+              name={kings[0].name}
+              img={atanagildo}
+              price={kings[0].price}
+              setTotal={setTotal}
+            />
+          </div>
+
+          <div className="card ervigio">
+            <NewComponent
+              name={kings[1].name}
+              img={ervigio}
+              price={kings[1].price}
+              setTotal={setTotal}
+            />
+          </div>
+          <div className="card ataulfo">
+            <NewComponent
+              name={kings[2].name}
+              img={ataulfo}
+              price={kings[2].price}
+              setTotal={setTotal}
+            />
+          </div>
+          <div className="card leogivildo">
+            <NewComponent
+              name={kings[3].name}
+              img={leogivildo}
+              price={kings[3].price}
+              setTotal={setTotal}
+            />
+          </div>
+          <div className="card recesvinto">
+            <NewComponent
+              name={kings[4].name}
+              img={recesvinto}
+              price={kings[4].price}
+              setTotal={setTotal}
+            />
+          </div>
+          <div className="card sisebuto">
+            <NewComponent
+              name={kings[5].name}
+              img={sisebuto}
+              price={kings[5].price}
+              setTotal={setTotal}
+            />
+          </div>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/NewComponent.jsx
+++ src/NewComponent.jsx
@@ -1,96 +1,20 @@
-import React, { useState } from "react";
 import "./App.css";
-import atanagildo from "./reyes/rey_atanagildo.png";
-import ervigio from "./reyes/rey_ervigio.png";
-import ataulfo from "./reyes/rey_ataulfo.png";
-import leogivildo from "./reyes/rey_leogivildo.png";
-import recesvinto from "./reyes/rey_recesvinto.png";
-import sisebuto from "./reyes/rey_sisebuto.png";
 
+function NewComponent({name,img,price,setTotal}) {
 
-const NewComponent = () => {
+  const calculate = (e) => {
+    e.target.parentNode.style.display='none';
+    setTotal((e)=>e+price);
+  };
 
-
-  const [total, setTotal] = useState(0)
-  const calculate = () => {
-    setTotal(128 + total)
-  }
-
-  const kings = [
-    {
-      name: "Atanagildo",
-      color: "darkolivegreen",
-      price: 178,
-    },
-    {
-      name: "Ervigio",
-      color: "crimson",
-      price: 169,
-    },
-    {
-      name: "Ataúlfo",
-      color: "peru",
-      price: 81,
-    },
-    {
-      name: "Leogivildo",
-      color: "darkmagenta",
-      price: 126,
-    },
-    {
-      name: "Recesvinto",
-      color: "royalblue",
-      price: 141,
-    },
-    {
-      name: "Sisebuto",
-      color: "teal",
-      price: 69,
-    },
-  ];
   return (
-    <>
-      <h2>Total to pay: ${total}</h2>
-      <div className="container">
-        <div className="card atanagildo">
-          <h2>King {kings[0].name}</h2>
-          <img src={atanagildo} alt="" />
-          <p>Price: ${kings[0].price}</p>
+      <>  
+          <h2>King {name}</h2>
+          <img src={img} alt="" />
+          <p>Price: ${price}</p>
           <button onClick={calculate}>Buy</button>
-        </div>
-        <div className="card ervigio">
-          <h2>King {kings[1].name}</h2>
-          <img src={ervigio} alt="" />
-          <p>Price: ${kings[1].price}</p>
-          <button onClick={calculate}>Buy</button>
-        </div>
-        <div className="card ataulfo">
-          <h2>King {kings[2].name}</h2>
-          <img src={ataulfo} alt="" />
-          <p>Price: ${kings[2].price}</p>
-          <button onClick={calculate}>Buy</button>
-        </div>
-        <div className="card leogivildo">
-          <h2>King {kings[3].name}</h2>
-          <img src={leogivildo} alt="" />
-          <p>Price: ${kings[3].price}</p>
-          <button onClick={calculate}>Buy</button>
-        </div>
-        <div className="card recesvinto">
-          <h2>King {kings[4].name}</h2>
-          <img src={recesvinto} alt="" />
-          <p>Price: ${kings[4].price}</p>
-          <button onClick={calculate}>Buy</button>
-        </div>
-        <div className="card sisebuto">
-          <h2>King {kings[5].name}</h2>
-          <img src={sisebuto} alt="" />
-          <p>Price: ${kings[5].price}</p>
-          <button onClick={() => { alert('Hola') }}>Buy</button>
-        </div>
-      </div>
-    </>
+        </>
   );
-};
+}
 
 export default NewComponent;
